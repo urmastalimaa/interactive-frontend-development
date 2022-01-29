148759d4959025576d29b3350ffd2192f1e09200
Stanislav <stanislav@qminderapp.com>
2022-01-29 18:12:43 +0200

    refactor: use jest for lecture 6

--- lecture_6/src/index.js
+++ lecture_6/src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom";
-import {App} from "./websocket_basics/App";
+import { App } from "./websocket_basics/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
