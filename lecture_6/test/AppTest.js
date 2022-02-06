54115a6f3ba02f0d0ebe55696b6a3412296cfebf
Stanislav Deviatykh <stanislav@qminderapp.com>
2022-02-06 16:20:21 +0200

    refactor: use jest for lecture 6 (#19)

--- lecture_6/test/AppTest.js
+++ lecture_6/test/AppTest.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import Sinon from "sinon";
-import {App} from "../src/websocket_basics/App";
+import { act } from "react-dom/test-utils";
+import { App } from "../src/websocket_basics/App";
 
 /*
  * Instead of pulling in a dependency to mock out an interface such as
@@ -9,58 +9,50 @@ import {App} from "../src/websocket_basics/App";
  * The UI components should not rely on the intimate details of the WebSocket
  * interface anyway.
  */
-const createMockWebSocketInterface = () => {
-  /*
-   * Create a named "stub" function, the behaviour of which can be later
-   * controlled.
-   */
-  const connectWebSocket = Sinon.stub().named('connect');
-
-  /*
-   * Return value of our WebSocket interface allows closing the connection.
-   * When called, immediately yield to the onClose argument provided to
-   * `connectWebSocket`.
-   */
-  const close = Sinon.stub().named('close').callsFake(() => {
-    connectWebSocket.yieldTo('onClose', {reason: null});
-  });
-
-  /*
-   * When connect is called, let the connection be established on the _next
-   * event loop_ as in reality, the connection is never established
-   * _immediately_.
-   */
-  connectWebSocket.callsFake(() => {
+const createMockWebSocketInterface = () =>
+  jest.fn().mockImplementation(({ onOpen, onClose }) => {
     /*
+     * Return value of our WebSocket interface allows closing the connection.
+     * When called, immediately yield to the onClose argument provided to
+     * `connectWebSocket`.
+     */
+    const wsConnection = {
+      close: () => onClose({ reason: null }),
+    };
+
+    /*
+     * When connect is called, let the connection be established on the _next
+     * event loop_ as in reality, the connection is never established
+     * _immediately_.
+     *
      * `setTimeout` with delay argument 0, or without the second argument
      * altogether invokes the callback function asynchronously, on the next
      * event loop - i.e. with minimal delay.
      */
     setTimeout(() => {
-      connectWebSocket.yieldTo("onOpen");
+      onOpen();
     });
 
     // Return value of the stub function
-    return ({close});
+    return wsConnection;
   });
 
-  return connectWebSocket;
-};
-
-describe('App', () => {
-  it("renders", async function() {
+describe("App", () => {
+  test("renders", async function () {
     const connectWebSocket = createMockWebSocketInterface();
 
-    render(<App connectWebSocket={connectWebSocket}/>);
+    render(<App connectWebSocket={connectWebSocket} />);
     screen.getByText("Connection: Connecting");
 
     await screen.findByText("Connection: Connected");
     screen.getByText("Pings received: 0");
 
-    connectWebSocket.yieldTo("onMessage", {type: "ping", event: {pingCount: 2}});
+    const { onMessage } = connectWebSocket.mock.calls[0][0];
+
+    act(() => onMessage({ type: "ping", event: { pingCount: 2 } }));
     screen.getByText("Pings received: 2");
 
-    connectWebSocket.yieldTo("onMessage", {type: "ping", event: {pingCount: 3}});
+    act(() => onMessage({ type: "ping", event: { pingCount: 3 } }));
     // The example App should disconnect on the third ping
     await screen.findByText("Connection: Disconnected");
 
@@ -73,12 +65,12 @@ describe('App', () => {
      * made longer by 2.5 seconds every time it runs.
      * What can be done to remedy this situation?
      */
-    this.timeout(5000); // eslint-disable-line no-invalid-this
+    jest.setTimeout(5000); // eslint-disable-line no-invalid-this
 
     // We must also increase the timeout for the `findBy` selector as default
     // timeout is 1000
     // (https://testing-library.com/docs/dom-testing-library/api-async#waitfor)
-    await screen.findByText("Connection: Connected", {}, {timeout: 3000});
+    await screen.findByText("Connection: Connected", {}, { timeout: 3000 });
     screen.getByText("Pings received: 0");
   });
 });
